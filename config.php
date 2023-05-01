f13dfd3a67ab6bf52534e95dda9c78ebe23bc4fc
Vinayak Pandey <67498083+geekyvinayak@users.noreply.github.com>
2023-05-01 18:27:24 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-define('DB_SERVER', 'db4free.net:3306');
-define('DB_USERNAME', 'vinayakdb');
-define('DB_PASSWORD', 'vinayak@12');
-define('DB_NAME', 'icases');
+define('DB_SERVER', '**');
+define('DB_USERNAME', '**');
+define('DB_PASSWORD', '**');
+define('DB_NAME', '**');
  
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
